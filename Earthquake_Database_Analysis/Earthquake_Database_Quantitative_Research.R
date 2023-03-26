4e83cf9c6e061aa70febde1a211624f5f5188678
NikaDumbo <nika.dumbadze@mail.ru>
2023-03-26 21:39:47 +0400

    Quantitative research for February data

--- Earthquake_Database_Analysis/Earthquake_Database_Quantitative_Research.R
+++ Earthquake_Database_Analysis/Earthquake_Database_Quantitative_Research.R
@@ -15,3 +15,18 @@ paste("Mean Value: ", mean(mag))
 paste("Standart Deviation Value: ", sd(mag))
 paste("Variance Value: ", var(mag))
 paste("Median Value: ", median(mag))
+
+# Get only February data
+# In this case, we have data for February and March 2023
+febData = subset(data, as.Date(data$time) < as.Date("2023-03-01"))
+magf <- na.omit(febData$mag) # get magnitude data, exclude NA
+
+# Quantitative research for February data
+paste("Min Value: ", min(magf))
+paste("Max Value: ", max(magf))
+paste("Sum Value: ", sum(magf))
+paste("Count Value: ", length(magf))
+paste("Mean Value: ", mean(magf))
+paste("Standart Deviation Value: ", sd(magf))
+paste("Variance Value: ", var(magf))
+paste("Median Value: ", median(magf))
